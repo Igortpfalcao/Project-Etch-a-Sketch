07db738e25fbaba6cd66ba02f4d00128b3066857
Igor <rogianilorac@gmail.com>
2024-10-18 11:41:12 -0300

    hovering now working

--- etch.js
+++ etch.js
@@ -4,10 +4,21 @@ const generate = document.querySelector("button")
 
 generate.addEventListener("click", () => createGrid(parseInt(document.querySelector(".slider").value)));
 
+const colors = [];
+const generatecolors = function(){
+    for (let white = 0; white <= 255; white += 51){
+        colors.unshift("rgb(" + white + ", " + white + ", " + white + ")");
+    }
+}
+
+generatecolors();
+
+let currentindex = 0;
 
 const createGrid = function(scale){
     
     container.innerHTML = '';
+    currentindex = 0;
     
     for (let row = 0; row < scale; row++){
         let div = document.createElement("div");
@@ -15,10 +26,22 @@ const createGrid = function(scale){
         for (let column = 0; column < scale; column++){
             let internal = document.createElement("div");
             internal.style.cssText = "display: flex; border: solid 1px black; flex: 1; height: 100%;"
+            internal.addEventListener("mouseover", () => hovering(internal));
             div.appendChild(internal);
         }
         
         container.appendChild(div);
     }
 
+}
+
+const hovering = function(square){
+    if (currentindex < 6){
+        square.style.backgroundColor = colors[currentindex];
+        currentindex += 1;
+    }
+    else{
+        currentindex = 0
+        square.style.backgroundColor = colors[currentindex];
+    }
 }
